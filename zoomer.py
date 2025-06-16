24c68c897b88420f8872e62b2d4cc7e3a9872c1d
mac <mac@SNYPER-MAC.local>
2025-06-16 02:21:13 -0400

    approve and confirm

--- zoomer.py
+++ zoomer.py
@@ -79,21 +79,17 @@ def approve_remote_control():
   bring_zoom_window_to_top("control")
   time.sleep(1)
   approve_position = find_text_in_screen('Approve', 400)
-  if approve_position is None:
-    print('Approve button not found')
-    return
-  x, y = approve_position
-  move_mouse_smoothly(x, y, duration=0.5)
-  click(x, y)
+  if approve_position is not None:
+    x, y = approve_position
+    move_mouse_smoothly(x, y, duration=0.5)
+    click(x, y)
 
   time.sleep(0.5)
   confirm_position = find_text_in_screen('Confirm', 300)
-  if confirm_position is None:
-    print('Confirm button not found')
-    return
-  x, y = confirm_position
-  move_mouse_smoothly(x, y, duration=0.5)
-  click(x, y)
+  if confirm_position is not None:
+    x, y = confirm_position
+    move_mouse_smoothly(x, y, duration=0.5)
+    click(x, y)
 
 def share_screen():
   bring_zoom_window_to_top("Zoom Meeting")
